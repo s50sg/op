70dac4fdfb284aab531a8db085dacda521f48574
s50sg <ziga.bahoric22@gmail.com>
2021-11-05 16:56:58 +0100

    vaja2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # op
+
+naloge iz osnov programiranja
---
+++ vaja2.js
@@ -0,0 +1,27 @@
+function sumDeviders(a){
+  var t = 0;
+  for(var i = 0; i <= a/2; i++){
+    if (a % i == 0){
+      t = t + i;
+    }
+  }
+  return t;
+}
+function fndNum(a, b){
+  a_sum = sumDeviders(a);
+  b_sum = sumDeviders(b);
+
+  if (a == b_sum && b == a_sum){
+    console.log(a)
+    console.log(b)
+  }
+}
+function  main(){
+  for(i = 100; i <= 10000; i++){
+    for(j = 100; j <= 10000; j++){
+      fndNum(i, j);
+      console.log(i, j, sumDeviders(j));
+    }
+  }
+}
+main();
