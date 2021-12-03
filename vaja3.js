086e9b53a89f57700b76429d5b20c4e18db05d87
s50sg <56229558+s50sg@users.noreply.github.com>
2021-12-03 03:36:09 +0100

    vaja3 koncana

--- vaja3.js
+++ vaja3.js
@@ -1,10 +1,52 @@
 function loto() {
   var arr = [0];
-  for (var i = 0; i <= 6; i++) {
-    arr[i] = Math.floor(Math.random() * 39) + 1;
-    console.log(i, arr[i]);
+  for (var a = 0; a <= 6; a++) {
+    arr[a] = Math.floor(Math.random() * 39) + 1;
+    for (i = a - 1; i >= 0; i-- ) {
+      while (arr[a] == arr[i]) {
+        arr[a] ++;
+        i = a - 1
+      }
+    }
   }
-  console.log(arr);
+  return arr;
 }
 
-loto();
+function swap(arr, a, b) {
+  var tmp = [0];
+  tmp[a] = arr[a];
+  arr[a] = arr[b];
+  arr[b] = tmp[a];
+}
+
+function sort(arr) {
+  var i = 0, j = 0;
+  while(j <= 36) {
+    while (i < 7) {
+      if (arr[i] > arr[i + 1]) {
+        swap(arr, i, i + 1);
+      }
+      else j++;
+      i++;
+    }
+    i = 0;
+  }
+}
+
+function check(arr) {
+  var i = 0;
+  while(arr[i] != arr[i + 1] && i <= 5) {
+    i++;
+  }
+  if (i == 6) return 1;
+  else return 0;
+}
+
+z = 0;
+do {
+  a = loto();
+  sort(a);
+  b = check(a);
+  console.log(a, b, z);
+  z++;}
+while(b == 1 && z < 1000);
